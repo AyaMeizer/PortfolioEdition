ff95f971b76717e2ce13d0a92689fe623c19fc88
AyaMeizer <90200721+AyaMeizer@users.noreply.github.com>
2021-10-23 14:44:43 +0300

    portfolio edition

---
+++ LIGHT-DARK MOOD/darkmood.js
@@ -0,0 +1,9 @@
+const dark = document.querySelector("#dark-toggle");
+
+function darkmood(id) {
+    document.body.classList.toggle("dark");
+}
+function lightmood(item){
+    item.innerHTML='Light Mood';
+    
+}
